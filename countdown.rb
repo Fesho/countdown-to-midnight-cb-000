b44e79d19df0e667d6aadf3c79aeeb1769319b89
Mostafa Elfishawy <fisho_world@yahoo.com>
2019-01-06 15:57:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,33 +1,22 @@
-  
-  def countdown(second)
+def countdown(num)
 
-  while second > 1
-  
-  second -= 1
-  
-   puts " SECOND(S) #{second}."
-  end
-     return "HAPPY NEW YEAR!"
+ while num > 0
+
+   puts "#{num} SECOND(S)!"
+  num -= 1
+
+ end
+  "HAPPY NEW YEAR!"
 end
 
-countdown(11)
 
+ def countdown_with_sleep(num)
 
+ while num > 0
 
+   puts "#{num} SECOND(S)!"
+  sleep(1)
+  num -= 1
 
-def countdown_with_sleep(second)
-  while second > 1
-  
-  second -= 1
-  
-   puts " SECOND(S) #{second}."
- sleep(1)
-
-  end
-
-return "HAPPY NEW YEAR!"
-
-end
-
-countdown_with_sleep(10)
-  
+ end
+  "HAPPY NEW YEAR!"
